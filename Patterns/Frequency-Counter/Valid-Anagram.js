a277a0350c3a3a9e491942d6686293c5925bfbb6
totsuka1 <takumi.otsuka@gmail.com>
2021-12-29 20:23:27 -0600

    Anagram whiteboard code (untested)

--- Patterns/Frequency-Counter/Valid-Anagram.js
+++ Patterns/Frequency-Counter/Valid-Anagram.js
@@ -2,7 +2,34 @@
 //Assumptions: all lowercase, single words (no spaces), no special characters
 
 function validAnagram(string1, string2) {
-  //solution code goes here
+  //Case for if strings are of different lengths (return false)
+  if (string1.length !== string2.length) {
+    return false;
+  }
+
+  //instantiate empty objects to store character counts
+  let string1counter = {};
+  let string2counter = {};
+
+  //iterate over string1, adding to the count for the character in its object
+  for (let char of string1) {
+    string1counter[char] = (string1counter[char] || 0) + 1;
+  }
+
+  //iterate over string2, adding to the count for the character in its object
+  for (let char of string2) {
+    string2counter[char] = (string2counter[char] || 0) + 1;
+  }
+
+  //iterate over the counter object for string1, and compare values for each key with those in counter object for string 2. If counts aren't equivalent, return false
+  for (let charKey in string1counter) {
+    if (string1counter[charKey] !== string2counter[charKey]) {
+      return false;
+    }
+  }
+
+  //if successfully iterated over counter objects without breaking, return true
+  return true;
 }
 
 //Test Cases
